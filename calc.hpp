0441c87d7b15cb963a87f445daf4d58f7fa09102
KTete-jpg <55944083+KTete-jpg@users.noreply.github.com>
2020-02-29 19:50:13 +0200

    Create calc.hpp

---
+++ calc.hpp
@@ -0,0 +1,22 @@
+#ifndef CALC_HPP
+#define CALC_HPP
+#include <iostream>
+#include "getImputNumber.hpp"
+
+int add(int x,int y){
+
+    return x+y;
+}
+int diff(int x,int y){
+
+    return x-y;
+}
+int mult(int x,int y){
+
+    return x*y;
+}
+float divise(int x, int y){
+
+    return x/y;
+}
+#endif
