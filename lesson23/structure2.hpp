b38f3a754fbba57a840ed011dfed8262fc272988
KTete-jpg <55944083+KTete-jpg@users.noreply.github.com>
2020-03-08 15:07:09 +0300

    Create structure2.hpp

---
+++ lesson23/structure2.hpp
@@ -0,0 +1,11 @@
+#ifndef STRUCT2_HPP
+#define STRUCT2_HPP
+#include <cstring>
+typedef struct
+{
+  std::string marka;
+  int speed;
+
+} car; //определения типа структуры(сложного типа данных) person{параметры}
+
+#endif
