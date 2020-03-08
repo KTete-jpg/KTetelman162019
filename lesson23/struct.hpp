7e56a7209a092d6daa5a48185b69182be5d8c453
KTete-jpg <55944083+KTete-jpg@users.noreply.github.com>
2020-03-08 15:06:44 +0300

    Create struct.hpp

---
+++ lesson23/struct.hpp
@@ -0,0 +1,16 @@
+#ifndef STRUCT_HPP
+#define STRUCT_HPP
+#include <cstring>
+typedef struct
+{
+  std::string name;
+  int age;
+  int height;
+  int weight;
+  std::string country;
+  bool married;
+
+
+} person; //определения типа структуры(сложного типа данных) person{параметры}
+
+#endif
